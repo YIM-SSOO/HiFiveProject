c60c050e458502b61b14fd920da26ce5e40ef365
YIM-SSOO <81502053+YIM-SSOO@users.noreply.github.com>
2021-09-20 21:02:30 +0900

    Initial commit

---
+++ src/main/java/hifive/hifivespring/HifiveSpringApplication.java
@@ -0,0 +1,13 @@
+package hifive.hifivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HifiveSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HifiveSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hifive/hifivespring/HifiveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hifive.hifivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HifiveSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
